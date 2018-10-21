23e0d77fcbaeab93729bad9206b0a6fd3d838117
Justin <info@allcrypt.tech>
2018-10-21 08:27:27 +0300

    ki9
    ki9

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -58,7 +58,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1527163200, // * UNIX timestamp of last checkpoint block
-    0,    // * total number of transactions between genesis and last checkpoint
+    1,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     100        // * estimated number of transactions per day after checkpoint
 };
